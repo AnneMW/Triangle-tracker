5b8e3083e6a3dab2d0cbfd8b1da5286ef9ff91d0
Anne <annemwaniki94@gmail.com>
2019-04-28 13:10:01 +0300

    add javascript function

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-function triangle(){
-  var A = parseInt(document.getElementById('a').value);
-  var B = parseInt(document.getElementById('b').value);
-  var C = parseInt(document.getElementById('c').value);
-  if (A+B<=C || A+C<=B || B+C<=A) {
-    alert("This is not a triangle.");
-  }
-  else if ( A !== B && B !== C && A !== C ){
-  alert("This is a scalene triangle.");
-  }
-    else if ( A===B && B===C ) {
-    alert("This is an equilateral triangle.");
-    }
-      else if ( A===B || A===C || B===C ){
-      alert("This is an isosceles triangle.");
-        }
-          else {
-            alert("A triangle has three sides.");
-          }
+function triangle() {
+	var A = parseInt(document.getElementById('a').value);
+	var B = parseInt(document.getElementById('b').value);
+	var C = parseInt(document.getElementById('c').value);
+	if (A + B <= C || A + C <= B || B + C <= A) {
+		alert("This is not a triangle.");
+	} else if (A !== B && B !== C && A !== C) {
+		alert("This is a scalene triangle.");
+	} else if (A === B && B === C) {
+		alert("This is an equilateral triangle.");
+	} else if (A === B || A === C || B === C) {
+		alert("This is an isosceles triangle.");
+	} else {
+		alert("A triangle has three sides.");
+	}
 }
